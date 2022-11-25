156d2c36facce82658efb810e6f90ba582ccb5e0
Ingrid Hernandez <ingrid_Hernandez@unitec.edu>
2022-11-25 13:38:27 -0600

    class usuario

--- src/lab7p2_ingridhernandez_12141186/Principio.java
+++ src/lab7p2_ingridhernandez_12141186/Principio.java
@@ -4,12 +4,15 @@
  */
 package lab7p2_ingridhernandez_12141186;
 
+import java.util.Random;
+import javax.swing.DefaultComboBoxModel;
+
 /**
  *
  * @author ADMINISTRADOR1
  */
 public class Principio extends javax.swing.JFrame {
-
+ static Futbol a = new Futbol();
     /**
      * Creates new form Principio
      */
@@ -26,18 +29,68 @@ public class Principio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jUserInput = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jPasswordInput = new javax.swing.JPasswordField();
+        jIngresar = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 533, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setText("Login");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel2.setText("Usuario");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel3.setText("Contraseña");
+
+        jIngresar.setText("Ingresar");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(126, 126, 126)
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel2)
+                            .addComponent(jUserInput)
+                            .addComponent(jLabel3)
+                            .addComponent(jPasswordInput, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(101, 101, 101)
+                        .addComponent(jIngresar)))
+                .addContainerGap(99, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 413, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jUserInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jPasswordInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(jIngresar)
+                .addContainerGap(50, Short.MAX_VALUE))
         );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 330, 300));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -76,7 +129,37 @@ public class Principio extends javax.swing.JFrame {
             }
         });
     }
+      public static int idGenerator() {
+        Random rnd = new Random();
+        int id = rnd.nextInt(1000);
+        boolean centinela = false;
+        while (centinela == true) {
+            int cont = 0;
+            DefaultComboBoxModel lista = a.getListaJugadores();
+            for (int i = 0; i < lista.getSize(); i++) {
+                if (lista.getElementAt(i) instanceof Usuario jugadores) {
+                    if (jugadores.getID() == id) {
+                        cont++;
+                    }
+                }
+            }
+            if (cont > 0) {
+                id = rnd.nextInt();
+            } else {
+                centinela = true;
+            }
+        }
+        return id;
+    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jIngresar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordInput;
+    private javax.swing.JTextField jUserInput;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/lab7p2_ingridhernandez_12141186/Usuario.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7p2_ingridhernandez_12141186;
+
+/**
+ *
+ * @author ADMINISTRADOR1
+ */
+public class Usuario {
+    private String nombreUsuario;
+    private int ID;
+    private String password;
+    private String tipo;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombreUsuario, int ID, String password, String tipo) {
+        this.nombreUsuario = nombreUsuario;
+        this.ID = ID;
+        this.password = password;
+        this.tipo = tipo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombreUsuario=" + nombreUsuario + ", ID=" + ID + ", password=" + password + ", tipo=" + tipo + '}';
+    }
+    
+}
